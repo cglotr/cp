66af20fcba8101ed2f54a5b9e947a98b0c67161c
Amir Ariffin <22372978+cglotr@users.noreply.github.com>
2022-06-11 14:01:19 +0800

    Create numb3r5.py

---
+++ leetcodeweeklycontest/282/Q2/numb3r5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        lost = {}
+        people = set()
+        
+        for i, j in matches:
+            people.add(i)
+            people.add(j)
+        
+        for i in people:
+            lost[i] = 0
+        
+        for i, j in matches:
+            lost[j] += 1
+        
+        zero = []
+        one = []
+        
+        for i in people:
+            if lost[i] == 0:
+                zero.append(i)
+            elif lost[i] == 1:
+                one.append(i)
+        
+        zero.sort()
+        one.sort()
+        
+        return [zero, one]
